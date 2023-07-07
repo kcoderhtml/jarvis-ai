a77e9935191c603d832cc09953a958225ecb0236
Kieran Klukas - kierank@Glaedrs-Nest.local <92754843+kcoderhtml@users.noreply.github.com>
2023-07-07 19:59:21 -0400

    :recycle: Refactor code in jarvis.py
    The code changes simplify the `command_lookup` dictionary by removing unnecessary commands. Additionally, the chat message sent to the GPT-3 model is updated to provide more accurate instructions to Jarvis.

--- jarvis.py
+++ jarvis.py
@@ -11,8 +11,6 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 wake_word = os.getenv("WAKE_WORD")
 
 command_lookup = {
-    "open terminal": ["open", "-a", "Terminal"],
-    "close terminal": ["osascript", "-e", 'quit app "Terminal"'],
     "conversation": ["echo", ""],
     "play discover weekly": ["osascript", "-e", 'tell application "Spotify" to play track "spotify:playlist:37i9dQZEVXcMaWCjUILjal"'],
     "exit": ["exit"],
@@ -51,7 +49,7 @@ def get_gpt(command):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "You are Jarvis, a snarky, artificial assistant created by Kieran Klukas. You will occasionally use dry humor. Based on the below input, select the best command to execute from the following array. Return only the command that should be executed from the array and a response as a JSON object" + commands_string},
+            {"role": "system", "content": "You are Jarvis, a dry, snarky, ai assistant. Based on the below input, select the best command to execute from the following array. Return only the command that should be executed from the array and a response as a JSON object. " + commands_string},
             {"role": "user", "content": "Input:" + command}
         ]
     )
@@ -59,7 +57,11 @@ def get_gpt(command):
     if response["choices"][0]["message"]["role"] == "assistant":
         gpt_response = response["choices"][0]["message"]["content"]
         print("GPT-3:", gpt_response)
-        assistant = json.loads(gpt_response)
+        try:
+            assistant = json.loads(gpt_response)
+        except json.decoder.JSONDecodeError:
+            print("Error decoding JSON response")
+            return
         say(assistant['response'])
     else:
         print("Jarvis: Sorry, I didn't understand that command.")
