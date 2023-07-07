8307de26ca94b21a1ac44f059246852fa453473a
Kieran Klukas - kierank@Glaedrs-Nest.local <92754843+kcoderhtml@users.noreply.github.com>
2023-07-07 17:09:25 -0400

    :recycle: Refactor code for speech recognition and wake word detection

--- jarvis.py
+++ jarvis.py
@@ -2,46 +2,15 @@ import speech_recognition as sr
 import subprocess
 import openai
 import json
-import pvporcupine
 import os
 from dotenv import load_dotenv
-import pyaudio
-import numpy as np
+
 
 load_dotenv()
 
-porcupine_api_key = os.getenv("PORCUPINE_API_KEY")
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-porcupine = pvporcupine.create(
-    access_key=porcupine_api_key,
-    keyword_paths=["Hey-Jarvis_en_mac_v2_2_0/Hey-Jarvis_en_mac_v2_2_0.ppn"]
-)
-
-pa = pyaudio.PyAudio()
-
-audio_stream = pa.open(
-        rate=porcupine.sample_rate,
-        channels=1,
-        format=pyaudio.paInt16,
-        input=True,
-        frames_per_buffer=porcupine.frame_length)
-
-def listen_for_wake_word():
-    print("Listening for wake word...")
-    pcm = audio_stream.read(porcupine.frame_length)
-    pcm = np.frombuffer(pcm, dtype=np.int16)
-
-    keyword_index = porcupine.process(pcm)
-
-    if keyword_index >= 0:
-        print('Wake word detected!')
-        
-        process_command(transcribe_audio())
-
-
 wake_word = "hey jarvis"
-
 
 commands = ["open terminal", "close terminal"]
 
@@ -51,24 +20,20 @@ commands_string = '[' + delimiter.join(map(str, commands)) + ']'  # Convert arra
 def transcribe_audio():
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        print("Speak now...")
-        # audio = r.listen(source)
-    try:
-        # text = r.recognize_google(audio)
-        return "text"
-    except sr.UnknownValueError:
-        print("Could not understand audio.")
-    except sr.RequestError as e:
-        print(f"Error: {e}")
-    return ""
+        print("Listening...")
+        audio = r.listen(source)
 
-def process_command(command):
     try:
-        command = command.lower()
+        text = r.recognize_google(audio)
+        # change text to lowercase for matching purposes
+        text = text.lower()
 
-        print("Heard:", command)
-        
-        listen_for_wake_word()
+        print("Heard:", text)
+        if "hey jarvis" in text:
+            # command = text.replace("hey jarvis", "").strip()
+            execute_command(text)
+        else:
+            print("Command not recognized.")
     except sr.UnknownValueError:
         print("Could not understand audio.")
     except sr.RequestError as e:
@@ -103,9 +68,6 @@ def execute_command(command):
 # Start listening for commands
 try:
     while True:
-        listen_for_wake_word()
+        transcribe_audio()
 except KeyboardInterrupt:
     print("Stopping...")
-    audio_stream.close()
-    porcupine.delete()
-    pa.terminate()
