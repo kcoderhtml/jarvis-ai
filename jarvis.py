f24302c2689f3551e287316ebbb8c446f427041f
Kieran Klukas - kierank@Glaedrs-Nest.local <92754843+kcoderhtml@users.noreply.github.com>
2023-07-12 14:38:04 -0400

    🚀 import speech and audio libraries: Import speech_recognition, struct, pyaudio, subprocess, pvporcupine in `jarvis.py` 🔧 set up command lookup array: Initialize the `command_lookup` dictionary in `jarvis.py` 🔐 load .env file: Load environment variables from the `.env` file in `jarvis.py` 🎙️ set up speech recognition: Set up speech recognition using `speech_recognition` library in `jarvis.py` 🌐 transcribe_audio(): Remove `transcribe_audio()` function definition in `jarvis.py` 📜 get_gpt(command): Change `get_gpt()` function to handle API response in `jarvis.py` 🔧 Start listening for commands: Add initialization process in `jarvis.py` 🔈 handle_response(response): Add `handle_response()` function to handle AI response in `jarvis.py` 🔄 Start listening for commands: Modify `while` loop for listening to hotword and recognizing audio in `jarvis.py` ❌ Stopping...: Remove unused code block in `jarvis.py`

--- jarvis.py
+++ jarvis.py
@@ -1,49 +1,62 @@
+# import speech and audio libraries
 import speech_recognition as sr
+import struct
+import pyaudio
 import subprocess
+import pvporcupine
+# import openai library
 import openai
+# import env libraries
 import json
 import os
 from dotenv import load_dotenv
 
+# load .env file
 load_dotenv()
-
 openai.api_key = os.getenv("OPENAI_API_KEY")
-wake_word = os.getenv("WAKE_WORD")
+porcupine_api_key = os.getenv("PORCUPINE_API_KEY")
 
+# set up command lookup array
 command_lookup = {
     "conversation": ["echo", ""],
     "play discover weekly": ["osascript", "-e", 'tell application "Spotify" to play track "spotify:playlist:37i9dQZEVXcMaWCjUILjal"'],
     "exit": ["exit"],
 }
-
 commands_string = str(list(command_lookup.keys()))  # Convert array to string
 
+# set up speech recognition
+r = sr.Recognizer()
+m = sr.Microphone()
+
+porcupine = None
+sound = None
+audio_stream = None
+
 def say(text):
+    print(text)
     subprocess.call(["say", text])
 
 def execute_command(command):
     subprocess.call(command)
 
-def transcribe_audio():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Listening...")
-        audio = r.listen(source)
-
+def handle_response(response):
     try:
-        text = r.recognize_google(audio)
-        # change text to lowercase for matching purposes
-        text = text.lower()
-
-        print("Heard:", text)
-        if wake_word in text:
-            get_gpt(text)
+        assistant = json.loads(response)
+    except json.decoder.JSONDecodeError:
+        say("Error decoding JSON response")
+        return
+    
+    if assistant['response'] is not None:
+        say(assistant['response'])
+    elif assistant['command'] is not None:
+        if assistant['command'] == ["exit"]:
+            print("Exiting...")
+            exit()
         else:
-            print("Command not recognized.")
-    except sr.UnknownValueError:
-        print("Could not understand audio.")
-    except sr.RequestError as e:
-        print(f"Error: {e}")
+            execute_command(assistant['command'])
+    else:
+        print("Command not recognized.")
+        return
 
 def get_gpt(command):
     response = openai.ChatCompletion.create(
@@ -57,31 +70,63 @@ def get_gpt(command):
     if response["choices"][0]["message"]["role"] == "assistant":
         gpt_response = response["choices"][0]["message"]["content"]
         print("GPT-3:", gpt_response)
-        try:
-            assistant = json.loads(gpt_response)
-        except json.decoder.JSONDecodeError:
-            print("Error decoding JSON response")
-            return
-        say(assistant['response'])
+        handle_response(gpt_response)
     else:
-        print("Jarvis: Sorry, I didn't understand that command.")
-        say("Sorry, I didn't understand that command.")
-
-    # Perform the corresponding action based on the assistant's response
-    command = command_lookup.get(assistant['command'])
-    if command is not None:
-        if command == ["exit"]:
-            print("Exiting...")
-            exit()
-        else:
-            execute_command(command)
-    else:
-        print("Command not recognized.")
-        return
+        say("Sorry, something went wrong with my brain.")
 
 # Start listening for commands
 try:
+    say("Initializing...")
+    porcupine = pvporcupine.create(
+        access_key=porcupine_api_key,
+        keyword_paths=["Hey-Jarvis_en_mac_v2_2_0/Hey-Jarvis_en_mac_v2_2_0.ppn"]
+        )
+
+    sound = pyaudio.PyAudio()
+    
+    audio_stream = sound.open(
+                    rate=porcupine.sample_rate,
+                    channels=1,
+                    format=pyaudio.paInt16,
+                    input=True,
+                    frames_per_buffer=porcupine.frame_length)
+    
+    with m as source: r.adjust_for_ambient_noise(source)
+    
+    listening = False
+    
+    say("Initialized")
     while True:
-        transcribe_audio()
+        pcm = audio_stream.read(porcupine.frame_length)
+        pcm = struct.unpack_from("h" * porcupine.frame_length, pcm)
+
+        keyword_index = porcupine.process(pcm)
+        
+        if keyword_index >= 0:
+            listening = True
+            print("Hotword Detected")
+            
+        if listening:
+            with m as source: audio = r.listen(source)
+            try:
+                value = r.recognize_google(audio)
+                print("You said {}".format(value))
+            except sr.UnknownValueError:
+                print("Oops! Didn't catch that")
+            except sr.RequestError as e:
+                print("Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))
+            finally:
+                listening = False
+            
 except KeyboardInterrupt:
-    print("Stopping...")
+    print("Stopping....")
+
+finally:
+    if porcupine is not None:
+        porcupine.delete()
+
+    if audio_stream is not None:
+        audio_stream.close()
+
+    if sound is not None:
+        sound.terminate()
