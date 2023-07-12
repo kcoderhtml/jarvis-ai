a11e8a8c1f543dc2b618ad02643d8e42845ba059
Kieran Klukas - kierank@Glaedrs-Nest.local <92754843+kcoderhtml@users.noreply.github.com>
2023-07-12 17:19:59 -0400

    ✨ Remove Porcupine wake word detection
    The Porcupine wake word detection functionality is no longer needed. :fire:

--- jarvis.py
+++ jarvis.py
@@ -3,7 +3,6 @@ import speech_recognition as sr
 import struct
 import pyaudio
 import subprocess
-import pvporcupine
 # import openai library
 import openai
 # import env libraries
@@ -14,7 +13,7 @@ from dotenv import load_dotenv
 # load .env file
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
-porcupine_api_key = os.getenv("PORCUPINE_API_KEY")
+wake_word = os.getenv("WAKE_WORD")
 
 # set up command lookup array
 command_lookup = {
@@ -27,10 +26,6 @@ commands_string = str(list(command_lookup.keys()))  # Convert array to string
 # set up speech recognition
 r = sr.Recognizer()
 m = sr.Microphone()
-
-porcupine = None
-sound = None
-audio_stream = None
 
 def say(text):
     print(text)
@@ -74,66 +69,34 @@ def get_gpt(command):
     else:
         say("Sorry, something went wrong with my brain.")
 
+say("Initializing...")
+
 # Start listening for commands
 try:
-    say("Initializing...")
-    porcupine = pvporcupine.create(
-        access_key=porcupine_api_key,
-        keyword_paths=["Hey-Jarvis_en_mac_v2_2_0/Hey-Jarvis_en_mac_v2_2_0.ppn"]
-    )
-
-    sound = pyaudio.PyAudio()
-
-    audio_stream = sound.open(
-        rate=porcupine.sample_rate,
-        channels=1,
-        format=pyaudio.paInt16,
-        input=True,
-        frames_per_buffer=porcupine.frame_length)
-
-    # with m as source:
-    #     r.adjust_for_ambient_noise(source)
-
-    listening = False
-    
-    say("Initialized")
+    with m as source:
+        r.adjust_for_ambient_noise(source)
+    say("Initialized. Listening for wake word...")
     while True:
-        pcm = audio_stream.read(porcupine.frame_length, exception_on_overflow=False)
-        pcm = struct.unpack_from("h" * porcupine.frame_length, pcm)
-
-        keyword_index = porcupine.process(pcm)
-        
-        if keyword_index >= 0:
-            listening = True
-            print("Hotword Detected")
-            
-        if listening:
             with m as source:
-                audio = r.listen(source, phrase_time_limit=5)
+                audio = r.listen(source, phrase_time_limit=2)
             try:
                 value = r.recognize_google(audio)
-                print("You said {}".format(value))
+                if wake_word in value.lower():
+                    say("Yes?")
+                    with m as source:
+                        audio = r.listen(source, phrase_time_limit=5)
+                    try:
+                        value = r.recognize_google(audio)
+                        get_gpt(value)
+                    except sr.UnknownValueError:
+                        print("Oops! Didn't catch that command")
+                    except sr.RequestError as e:
+                        print("Uh oh! Couldn't request results from Google Speech Recognition")
             except sr.UnknownValueError:
-                print("Oops! Didn't catch that")
+                print("Oops! Didn't catch that wake word")
             except sr.RequestError as e:
                 print("Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))
-            finally:
-                listening = False
-                value = r.recognize_google(audio)
-                print("You said {}".format(value))
-                break
-            
 except KeyboardInterrupt:
     print("Stopping....")
 except Exception as e:
     print("Error: ", e)
-
-finally:
-    if porcupine is not None:
-        porcupine.delete()
-
-    if audio_stream is not None:
-        audio_stream.close()
-
-    if sound is not None:
-        sound.terminate()
