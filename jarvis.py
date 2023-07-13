f4e47e324b659dd5c70eeaee240e986565a167a2
Kieran Klukas - kierank@Glaedrs-Nest.local <92754843+kcoderhtml@users.noreply.github.com>
2023-07-13 16:07:47 -0400

    :fire: switched wake word detection and audio device to pico

--- jarvis.py
+++ jarvis.py
@@ -1,18 +1,30 @@
-# import speech and audio libraries
-import speech_recognition as sr
 import subprocess
+# import speech library
 import pyttsx3
+# import speech recognition libraries
+import speech_recognition as sr
+import wave
+import tempfile
+import struct
+import numpy as np
+# import pvrecorder and porcupine libraries
+from pvrecorder import PvRecorder
+import pvporcupine
 # import openai library
 import openai
 # import env libraries
 import json
 import os
 from dotenv import load_dotenv
+# import time library
+import time
 
 # load .env file
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
+pico_api_key = os.getenv("PICO_API_KEY")
 wake_word = os.getenv("WAKE_WORD")
+tempfile_name = os.path.join(tempfile.gettempdir(), "command.wav")
 
 # set up command lookup array
 command_lookup = {
@@ -23,13 +35,53 @@ command_lookup = {
 commands_string = str(list(command_lookup.keys()))  # Convert array to string
 
 # set up speech recognition
-r = sr.Recognizer()
-m = sr.Microphone()
+porcupine = pvporcupine.create(
+    access_key=pico_api_key,
+    keyword_paths=["Hey-Jarvis_en_mac_v2_2_0/Hey-Jarvis_en_mac_v2_2_0.ppn"],
+)
+recognizer = sr.Recognizer()
 
 # set up text to speech
 engine = pyttsx3.init()
 engine.setProperty('rate', 190)
 
+def record_command():
+    command_recorder = PvRecorder(device_index=-1, frame_length=512)
+    audio = []
+
+    try:
+        command_recorder.start()
+
+        start_time = time.time()
+        silence_counter = 0  # Counter for consecutive frames with low volume
+        silence_threshold = 1000  # Adjust this threshold as needed
+
+        while time.time() - start_time < 5:
+            frame = command_recorder.read()
+            audio.extend(frame)
+
+            volume = np.abs(frame).mean()
+
+            # Check if volume is below threshold
+            if volume < silence_threshold:
+                silence_counter += 1
+            else:
+                silence_counter = 0
+
+            # Check if silence has persisted for 1 second
+            if silence_counter <= 1:
+                silence_time = time.time()  # Time of last frame with high volume
+            elif time.time() - silence_time > 1:
+                break
+
+        command_recorder.stop()
+        with wave.open(tempfile_name, 'w') as f:
+            f.setparams((1, 2, 16000, 512, "NONE", "NONE"))
+            f.writeframes(struct.pack("h" * len(audio), *audio))
+    finally:
+        command_recorder.delete()
+
+    
 def say(text):
     print(text)
     engine.say(text)
@@ -74,37 +126,48 @@ def get_gpt(command):
         say("Sorry, something went wrong with my brain.")
 
 say("Initializing...")
-# Start listening for commands
+
+# Initialize speech recognizer
+r = sr.Recognizer()
+
+recorder = PvRecorder(
+        frame_length=porcupine.frame_length,
+        device_index=-1)
+recorder.start()
+
+say("Initialized.")
 try:
-    with m as source:
-        r.adjust_for_ambient_noise(source)
-    say("Initialized.")
-    print("Say '" + wake_word + "' to wake me up.")
+    # Infinite loop for continuous listening
     while True:
-            with m as source:
-                audio = r.listen(source, phrase_time_limit=2)
-            try:
-                value = r.recognize_google(audio)
-                if wake_word in value.lower():
-                    say("Yes?")
-                    with m as source:
-                        audio = r.listen(source, phrase_time_limit=7)
-                    try:
-                        value = r.recognize_google(audio)
-                        print(value)
-                        say("thinking...")
-                        get_gpt(value)
-                    except sr.UnknownValueError:
-                        say("Oops! Didn't catch that.")
-                    except sr.RequestError as e:
-                        print("Uh oh! Couldn't request results from Google Speech Recognition")
-            except sr.UnknownValueError:
-                print("Oops! Didn't catch that wake word")
-            except sr.RequestError as e:
-                print("Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))
+        recorder.start()
+        print("Listening for wake word...")
+        
+        # Use pvrecorder to record audio until wake word is detected
+        while True:
+            pcm = recorder.read()
+            keyword_index = porcupine.process(pcm)
+            if keyword_index >= 0:
+                print("Wake word detected!")
+                recorder.stop()
+                break
+        # Use speech recognition after wake word is detected
+        print("Listening for command...")
+        record_command()
+        with sr.AudioFile(tempfile_name) as source:
+            audio = r.record(source)
+
+        try:
+            # Perform speech recognition
+            command = r.recognize_google(audio)
+            print(f"Command: {command}")
+            get_gpt(command)
+        except sr.UnknownValueError:
+            print("Could not understand audio")
+        except sr.RequestError as e:
+            print(f"Error: {str(e)}")
 except KeyboardInterrupt:
-    print("Stopping....")
-    engine.stop()
+    print("Stopping...")
+    recorder.stop()
+    recorder.delete()
+    porcupine.delete()
     exit()
-except Exception as e:
-    print("Error: ", e)
